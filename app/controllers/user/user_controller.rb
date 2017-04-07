ec726920a2bbce4a04a52abb9a0600ec89a53778
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-04-07 17:09:48 +0200

    Improved places selection / creation

--- app/controllers/user/user_controller.rb
+++ app/controllers/user/user_controller.rb
@@ -1,5 +1,12 @@
 class User::UserController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :check_entity
 
   layout 'user'
+
+  def check_entity
+    if current_user && current_user.legal_entity_id.nil?
+      # redirect_to
+    end
+  end
 end
