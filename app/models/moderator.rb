7cd353f887929619ae7dd27cc4246059a6ca2f7a
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-04-20 15:30:37 +0200

    Fix issue with users / moderators outside the Gd Lyon / Isere perimeter

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -27,6 +27,8 @@ class Moderator < ActiveRecord::Base
         GRAND_LYON
       elsif postal_code.start_with?('38') || postal_code.start_with?('73') || postal_code.start_with?('26')
         ISERE
+      else
+        GRAND_LYON
       end
     else
       GRAND_LYON
