1bed74b16fa34abe064ce9d6342c3a7138ca9e26
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-05-23 02:58:32 +0200

    Added license file

--- lib/transfer_pictures.rb
+++ lib/transfer_pictures.rb
@@ -10,7 +10,13 @@ class TransferPictures
       Dir.mkdir(item_reference.to_s)
       FOLDERS.each do |d|
         Dir.mkdir("#{item_reference}/#{d}")
-        file_name = d == 'thumb' ? file.gsub('.jpg', '.png') : file
+        if d == 'thumb'
+          file_name = file.gsub('.jpg', '.png')
+        elsif d != 'original'
+          file_name = file.split('.').first + '.jpg'
+        else
+          file_name = file
+        end
         FileUtils.cp("#{d}/#{file_name}", "#{item_reference}/#{d}/#{file_name}")
       end
     end
