1a1e78d74cda48153e4dde94d0dabe5ecfbab4ce
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-06-07 16:22:46 +0200

    Improved pictures transfer tool

--- lib/transfer_pictures.rb
+++ lib/transfer_pictures.rb
@@ -10,15 +10,17 @@ class TransferPictures
       Dir.mkdir(item_reference.to_s)
       FOLDERS.each do |d|
         Dir.mkdir("#{item_reference}/#{d}")
-        if d == 'thumb'
-          file_name = file.split('.').first + '.png'
-        elsif d != 'original'
-          file_name = file.split('.').first + '.jpg'
-        else
-          file_name = file
-        end
-        FileUtils.cp("#{d}/#{file_name}", "#{item_reference}/#{d}/#{file_name}")
       end
+    end
+    FOLDERS.each do |d|
+      if d == 'thumb'
+        file_name = file.split('.').first + '.png'
+      elsif d != 'original'
+        file_name = file.split('.').first + '.jpg'
+      else
+        file_name = file
+      end
+      FileUtils.cp("#{d}/#{file_name}", "#{item_reference}/#{d}/#{file_name}")
     end
   end
 end
