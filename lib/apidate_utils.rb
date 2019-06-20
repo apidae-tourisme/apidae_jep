5d68c4d1cf5d1e9dc345c7af0925d9087ac8a199
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-06-20 22:39:56 +0200

    apidae date utils

--- lib/apidate_utils.rb
+++ lib/apidate_utils.rb
@@ -2,12 +2,15 @@ class ApidateUtils
   def self.fix_apidate_openings(item_id)
     p = ProgramItem.find(item_id)
     ops = p.apidate_openings
-    ids_by_date = ops.group_by {|op| op['startDate']}
-                   .transform_values {|values| values.sort_by {|val| (1.0 / val["updatedAt"])}.first['externalId']}
-    local_remote_ids = {}
-    p.openings.each_pair do |date, opening_id|
-      local_remote_ids[opening_id] = ids_by_date[date] unless opening_id.blank?
+    current_ids = p.openings.values.select {|id| !id.blank?}
+    if (current_ids & ops.map {|op| op['externalId']}).blank?
+      ids_by_date = ops.group_by {|op| op['startDate']}
+                     .transform_values {|values| values.sort_by {|val| (1.0 / val["updatedAt"])}.first['externalId']}
+      local_remote_ids = {}
+      p.openings.each_pair do |date, opening_id|
+        local_remote_ids[opening_id] = ids_by_date[date] unless opening_id.blank?
+      end
+      p.update_remote_ids(local_remote_ids)
     end
-    p.update_remote_ids(local_remote_ids)
   end
 end
