4f6e98a9827f3b5502e30cb4427ebd78752236ce
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-06-20 22:22:33 +0200

    apidae date utils

--- lib/apidate_utils.rb
+++ lib/apidate_utils.rb
@@ -6,7 +6,7 @@ class ApidateUtils
                    .transform_values {|values| values.sort_by {|val| (1.0 / val["updatedAt"])}.first['externalId']}
     local_remote_ids = {}
     p.openings.each_pair do |date, opening_id|
-      local_remote_ids[opening_id] = ids_by_date[date]
+      local_remote_ids[opening_id] = ids_by_date[date] unless opening_id.blank?
     end
     p.update_remote_ids(local_remote_ids)
   end
