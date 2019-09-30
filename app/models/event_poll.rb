5e121624f653ab42c4d71d6dfd20764263e355d5
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-09-30 09:11:27 +0200

    event polls update

--- app/models/event_poll.rb
+++ app/models/event_poll.rb
@@ -23,19 +23,11 @@ class EventPoll < ActiveRecord::Base
   end
 
   def count_sat(offer_id)
-    offer_count('2018-09-15', offer_id)
+    offer_count('2019-09-21', offer_id)
   end
 
   def count_sun(offer_id)
-    offer_count('2018-09-16', offer_id)
-  end
-
-  def compare_sat(offer_id)
-    offer_count('2018-06-09', offer_id)
-  end
-
-  def compare_sun(offer_id)
-    offer_count('2018-06-10', offer_id)
+    offer_count('2019-09-22', offer_id)
   end
 
   def poll_general_comments(offer_id)
