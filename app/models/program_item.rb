b786b767325d896f1bcf89d317582a215e99986c
Jean-Baptiste Vilain <jbvilain@gmail.com>
2020-07-31 19:17:42 +0200

    fixed coords validation for saumur

--- app/models/program_item.rb
+++ app/models/program_item.rb
@@ -39,7 +39,7 @@ class ProgramItem < ActiveRecord::Base
   store :contact_data, accessors: [:telephone, :email, :website], coder: JSON
 
   validates_presence_of :item_type, :title, :main_place, :main_lat, :main_lng, :main_address, :main_town_insee_code,
-                        :main_transports, :summary, :covid_desc, :accessibility
+                        :main_transports, :summary, :accessibility
   validates :accept_pictures, acceptance: true
   validates :accept_safety, acceptance: true
   validates_length_of :summary, maximum: 255
