62725003b051ad8c0cc89c57adb39229911f0a88
Jean-Baptiste Vilain <jbvilain@gmail.com>
2021-07-28 12:52:13 +0100

    prevent new submissions for Isere

--- app/controllers/user/program_items_controller.rb
+++ app/controllers/user/program_items_controller.rb
@@ -1,9 +1,9 @@
 class User::ProgramItemsController < User::UserController
-  # before_action :check_auth
+  before_action :check_auth
   before_action :set_program_item, only: [:edit, :update, :show, :destroy, :confirm, :duplicate]
 
   def check_auth
-    if current_user && (current_user.territory == ISERE || current_user.territory == GRAND_LYON)
+    if current_user && current_user.territory == ISERE
       redirect_to user_dashboard_url, alert: "La saisie des offres pour les JEP 2021 n'est plus disponible en ligne."
     end
   end
