c3e08001300bb20bef750ada82188f18f0bab136
Jean-Baptiste Vilain <jbvilain@gmail.com>
2022-07-20 23:16:03 +0100

    isere affiche jep 2022

--- lib/batch_operations.rb
+++ lib/batch_operations.rb
@@ -7,7 +7,7 @@ class BatchOperations
 
     items.each do |item|
       decorate_item(item)
-      sleep(3)
+      sleep(2)
     end
   end
 
@@ -15,14 +15,20 @@ class BatchOperations
     item_files = item.attached_files.map {|af| af.picture_file_name}
 
     unless item_files.include?(POSTER_FILE_NAME)
-      Rails.logger.info "Decorating item #{item.id} - #{item.external_id}"
-      af = AttachedFile.new(picture: open(URI.parse("https://jep.apidae-tourisme.com/#{POSTER_FILE_NAME}")),
-                            credits: "Département de l'Isère")
-      af.picture.instance_write(:file_name, POSTER_FILE_NAME)
-      item.attached_files << af
-      item.save!
-      item.remote_save
-      Rails.logger.info "Finished decorating item #{item.id} - #{item.external_id}"
+      begin
+        Rails.logger.info "Decorating item #{item.id} - #{item.external_id}"
+        af = AttachedFile.new(picture: open(URI.parse("https://jep.apidae-tourisme.com/#{POSTER_FILE_NAME}")),
+                              credits: "Département de l'Isère")
+        af.picture.instance_write(:file_name, POSTER_FILE_NAME)
+        item.attached_files << af
+        item.save!
+        item.remote_save
+        Rails.logger.info "Finished decorating item #{item.id} - #{item.external_id}"
+      rescue OAuth2::Error => e
+        Rails.logger.error "OAuth2::Error for item #{item.id} - #{item.external_id}"
+      rescue Exception => e
+        Rails.logger.error "Exception caught for item #{item.id} - #{item.external_id}"
+      end
     end
   end
 end
