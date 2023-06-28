294e4cfa065114b7772ee70323bb7ef0a54c8e15
Jean-Baptiste Vilain <jbvilain@gmail.com>
2023-06-28 12:07:47 +0200

    fix api

--- app/models/program_item.rb
+++ app/models/program_item.rb
@@ -142,7 +142,7 @@ class ProgramItem < ActiveRecord::Base
   end
 
   def open_dates
-    openings.blank? ? [] : openings.select {|k, v| !v.blank? && !v.to_s.include?('-') && openings_details.map {|od| od['startDate']}.include?(k)}.keys
+    openings.blank? ? [] : openings.select {|k, v| !v.blank? && !v['id'].to_s.include?('-') && openings_details.map {|od| od['startDate']}.include?(k)}.keys
   end
 
   def validated_at
