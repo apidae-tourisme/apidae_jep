bb6879cd7d24238b393daf2b5cebf9d8301165d7
Jean-Baptiste Vilain <jbvilain@gmail.com>
2023-07-16 19:18:17 +0200

    isere 2023 poster

--- lib/batch_operations.rb
+++ lib/batch_operations.rb
@@ -1,8 +1,8 @@
 class BatchOperations
-  POSTER_FILE_NAME = 'affiche-jep-isere-2022.jpg'
+  POSTER_FILE_NAME = 'affiche-jep-isere-2023.jpg'
 
   def self.decorate_isere_events(apidae_ids = [])
-    items = ProgramItem.in_status(ISERE, 2022, ProgramItem::STATUS_VALIDATED)
+    items = ProgramItem.in_status(ISERE, 2023, ProgramItem::STATUS_VALIDATED)
     items = items.where(external_id: apidae_ids) unless apidae_ids.blank?
 
     items.each do |item|
