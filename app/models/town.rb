d9cdc72b4b4ffdd4c21e1a8502a1d9a2a5b66fca
Jean-Baptiste Vilain <jbvilain@gmail.com>
2024-05-14 13:21:23 +0200

    JEP 2024 part 3

--- app/models/town.rb
+++ app/models/town.rb
@@ -12,8 +12,13 @@ class Town < ActiveRecord::Base
     towns_json = File.read(json_file)
     towns_hashes = JSON.parse(towns_json, symbolize_names: true)
     towns_hashes.each do |town_data|
-      Town.create!(name: town_data[:nom], postal_code: town_data[:codePostal], insee_code: town_data[:code],
-                   country: 'fr', external_id: town_data[:id])
+      town = Town.find_or_initialize_by(external_id: town_data[:id])
+      town.name = town_data[:nom]
+      town.postal_code = town_data[:codePostal]
+      town.insee_code = town_data[:code]
+      town.country = 'fr'
+      town.description = town_data[:complement]
+      town.save!
     end
     result
   end
