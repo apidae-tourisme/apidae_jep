9b3010445216b7b669e52b4ed8ad76db4db1acf0
Jean-Baptiste Vilain <jbvilain@gmail.com>
2024-07-18 19:07:07 +0200

    isere 2024 poster

--- lib/batch_operations.rb
+++ lib/batch_operations.rb
@@ -1,13 +1,13 @@
 class BatchOperations
-  POSTER_FILE_NAME = 'affiche-jep-isere-2023.jpg'
+  POSTER_FILE_NAME = 'affiche-jep-isere-2024.jpg'
 
   def self.decorate_isere_events(apidae_ids = [])
-    items = ProgramItem.in_status(ISERE, 2023, ProgramItem::STATUS_VALIDATED)
+    items = ProgramItem.in_status(ISERE, 2024, ProgramItem::STATUS_VALIDATED)
     items = items.where(external_id: apidae_ids) unless apidae_ids.blank?
 
     items.each do |item|
       decorate_item(item)
-      sleep(2)
+      sleep(1)
     end
   end
 
@@ -17,6 +17,10 @@ class BatchOperations
     unless item_files.include?(POSTER_FILE_NAME)
       begin
         Rails.logger.info "Decorating item #{item.id} - #{item.external_id}"
+        previous_poster = item.attached_files.find {|af| af.picture_file_name == 'affiche-jep-isere-2023.jpg'}
+        if previous_poster
+          item.attached_files.delete(previous_poster)
+        end
         af = AttachedFile.new(picture: open(URI.parse("https://jep.apidae-tourisme.com/#{POSTER_FILE_NAME}")),
                               credits: "Département de l'Isère")
         af.picture.instance_write(:file_name, POSTER_FILE_NAME)
