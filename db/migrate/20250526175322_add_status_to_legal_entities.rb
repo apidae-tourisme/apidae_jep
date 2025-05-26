9c2f066a836b7931dd578ae5c7d23fc906f5f8dd
Jean-Baptiste Vilain <jbvilain@gmail.com>
2025-05-26 19:54:42 +0200

    add status to legal entities for dups removal

--- db/migrate/20250526175322_add_status_to_legal_entities.rb
+++ db/migrate/20250526175322_add_status_to_legal_entities.rb
@@ -1,5 +1,6 @@
 class AddStatusToLegalEntities < ActiveRecord::Migration[5.2]
   def change
     add_column :legal_entities, :status, :string, default: 'active'
+    add_index :legal_entities, :status
   end
 end
