ef08dec61853e32dc1558c22e2900fe8eff81c32
Jean-Baptiste Vilain <jbvilain@gmail.com>
2025-06-02 12:17:26 +0200

    restore auto-expiration for testing

--- app/models/program_item.rb
+++ app/models/program_item.rb
@@ -347,16 +347,16 @@ class ProgramItem < ActiveRecord::Base
 
     form_data[:fields] = '["root"]'
     form_data[:root] ||= '{"type":"FETE_ET_MANIFESTATION"}'
-    # form_data['root.fieldList'] = openings.blank? ? '[]' : '["expiration.dateExpiration","expiration.expirationAction"]'
+    form_data['root.fieldList'] = openings.blank? ? '[]' : '["expiration.dateExpiration","expiration.expirationAction"]'
     form_data['root.fieldList'] = '[]'
 
     data_hash.each_pair do |k, v|
       converted_data = build_form_data(k, v)
       unless converted_data.empty?
         merged_data = safe_merge(extract_as_hash(form_data[:root]), converted_data)
-        # unless openings.blank?
-        #   merged_data[:expiration] = {dateExpiration: (Date.parse(openings.keys.sort.last) + 1.day).to_s, expirationAction: "MASQUER_AUTOMATIQUEMENT"}
-        # end
+        unless openings.blank?
+          merged_data[:expiration] = {dateExpiration: (Date.parse(openings.keys.sort.last) + 1.day).to_s, expirationAction: "MASQUER_AUTOMATIQUEMENT"}
+        end
         form_data[:root] = JSON.generate(merged_data)
         impacted_fields = data_fields(converted_data, Array.new)
         form_data['root.fieldList'] = merge_fields(form_data['root.fieldList'], impacted_fields)
